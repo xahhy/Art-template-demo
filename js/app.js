a0e546a38cce2df5dca16684791459c8684c1592
Your Name <you@example.com>
2017-06-10 13:15:25 +0800

    add AdminLTE resources

--- js/app.js
+++ js/app.js
@@ -1,21 +1,9 @@
 /**
  * Created by hhy on 2017/6/9.
  */
-// var source = '<ul>'
-//     +    '{{each list value i}}'
-//     +        '<li>索引 {{i + 1}} ：{{value}}</li>'
-//     +    '{{/each}}'
-//     + '</ul>';
-//
-// var render = template.compile('/template/each-video.html');
-// var src = "<ul>
-// {{each list value i}}
-// <li>索引 {{i + 1}} ：{{value}}</li>
-// {/each}}
-// </ul>";
-var api_url = 'http://1.8.90.47:8888/api?format=json';
 var info;
-// var api_url = 'http://localhost:8000/api?format=json';
+var api_url = 'http://localhost:8000/api?format=json';
+
 $.getJSON(api_url, {}, function (result) {
     console.log(result.results);
     info = result.results;
@@ -23,7 +11,6 @@ $.getJSON(api_url, {}, function (result) {
         items : info
     });
     $("#content").html(html);
-
 });
 // $.ajax({
 //     url:api_url,
